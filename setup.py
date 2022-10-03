d05d8716709494b117826d47508ea38e7a38ba94
domonik <schdruzzi@gmail.com>
2022-10-03 11:29:09 +0200

    removed vrna build

--- setup.py
+++ setup.py
@@ -9,65 +9,47 @@ from tools.build_dependencies import build_viennarna
 
 
 
-CONDA_BUILD = False
-SKIP_VIENNARNA = False
+prefix = os.getenv("CONDA_PREFIX")
+if prefix is None:
+    print("No conda environment found. Falling back to default installation path of ViennaRNA.\n"
+          "Consider using a conda environment instead")
+    prefix = "/usr/local"
 
 
-def check_vrna(prefix):
-    if not os.path.exists(os.path.join(prefix, "lib", "libRNA.a")):
-        print("RNAlib missing")
-        return False
-    return True
+extra_link_args = []
+include_dir = []
+_include = os.path.join(prefix, "include")
+_lib = os.path.join(prefix, "lib")
+extra_link_args += [f"-L{_lib}", f"-I{_include}"]
+include_dir += [_include] + [np.get_include()]
+svi = sys.version_info
+python_l = f"python{svi[0]}.{svi[1]}"
+
+if not os.path.exists(os.path.join(prefix, "lib", "libRNA.a")):
+    raise FileNotFoundError("Not able to find ViennaRNA RNAlib installation. This version of RNAdist requires ViennaRNA "
+                            "to be installed. You can easily install it using Conda:\n"
+                            "conda install -c bioconda viennarna"
+                            )
+if not os.path.exists(os.path.join(prefix, "lib", python_l, "site-packages", "RNA")):
+    raise ImportError("Not able to find ViennaRNA python package in your current environment."
+                      "Please install it e.g. via Conda\n"
+                        "conda install -c bioconda viennarna"
+)
 
 
-filtered_args = []
-for i, arg in enumerate(sys.argv):
-    if arg == "--conda-build":
-        CONDA_BUILD = True
-    elif arg == "--skip-viennaRNA":
-        SKIP_VIENNARNA = True
-    else:
-        filtered_args.append(arg)
-
-sys.argv = filtered_args
 svi = sys.version_info
 python_l = f"python{svi[0]}.{svi[1]}"
 py_include = get_paths()["include"]
-extra_link_args = []
-include_dir = []
-if not CONDA_BUILD:
-    print("Building ViennaRNA from source")
-    prefix = os.getenv("CONDA_PREFIX")
-    if prefix is None:
-        print("No conda environment found. Falling back to default installation.\n"
-              "You might need  root privileges for that\n"
-              "Consider using a conda environment instead")
-        prefix = "/usr/local"
-    _include = os.path.join(prefix, "include")
-    _lib = os.path.join(prefix, "lib")
-    extra_link_args += [f"-L{_lib}", f"-I{_include}"]
-    include_dir += [_include] + [np.get_include()]
-    if os.path.exists(os.path.join(prefix, "lib", "libRNA.a")):
-        print("assuming ViennaRNA is already built")
-        SKIP_VIENNARNA = True
-
-    if not SKIP_VIENNARNA:
-        build_viennarna(prefix)
-    else:
-        print("skipping ViennaRNA build")
-    assert check_vrna(prefix), "ViennaRNA was not successfully installed"
-
-
-
 
 extra_link_args += [f"-I{py_include}"]
 
 
-expected_distance_module = Extension(
+cp_exp_dist_extension = Extension(
     "CPExpectedDistance.c_expected_distance",
     sources=["CPExpectedDistance/c_expected_distance.c"],
     extra_link_args=extra_link_args + ["-lRNA", "-lpthread", "-lstdc++", "-fopenmp", "-lm", f"-l{python_l}", "-Wl,--no-undefined"],
     include_dirs=include_dir,
+
 )
 
 with open("README.md") as handle:
@@ -85,7 +67,7 @@ def main():
         author="Domonik",
         author_email="dominik.rabsch@gmail.com",
         include_package_data=True,
-        ext_modules=[expected_distance_module],
+        ext_modules=[cp_exp_dist_extension],
         install_requires=[
             "numpy",
         ]
